7f3f4c6f2c5cd238792c77683d8c71e59824ce18
italo_jose <italo3720523@faculdadecci.com.br>
2025-05-20 00:25:39 -0300

    Prevenção

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const router = require("./Screen/routes");
+const router = require("./Routes/routes");
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Recebendo requisição: ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(router);
 
 app.get("/", (req, res) => {
